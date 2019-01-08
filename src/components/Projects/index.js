b0ecf40dfbaea53b12d0c11fae331ecefd0724a9
Alan <alan-eng@yandex.ru>
2019-01-08 22:49:38 +0300

    wip

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,6 +3,7 @@ import ScrollableAnchor from "react-scrollable-anchor"
 import Modal from '../Modal';
 import superkassaImg from './superkassa.jpg';
 import runesImg from './runes.jpg';
+import tensesGeneratorImg from './tensesGenerator.jpg';
 import style from './index.css';
 
 class Projects extends Component {
@@ -14,12 +15,12 @@ class Projects extends Component {
         this.hideModal = this.hideModal.bind(this);
     };
 
-    showModal() {
-        this.setState({ modal: true });
+    showModal(name) {
+        this.setState({ modal: name });
     };
 
     hideModal() {
-        this.setState({ modal: false });
+        this.setState({ modal: '' });
     };
 
     render() {
@@ -36,7 +37,7 @@ class Projects extends Component {
                         <button
                             className={style.btnCard}
                             type="button"
-                            onClick={this.showModal}
+                            onClick={() => this.showModal('superkassa')}
                         >
                             SHOW
                         </button>
@@ -49,22 +50,55 @@ class Projects extends Component {
                         <button
                             className={style.btnCard}
                             type="button"
-                            onClick={this.showModal}
+                            onClick={() => this.showModal('runes')}
+                        >
+                            SHOW
+                        </button>
+                    </div>
+                </div>
+                <div className={style.card}>
+                    <img src={tensesGeneratorImg} className={style.projectImg} alt="english tenses site" />
+                    <p className={style.titleCard}>nord runes</p>
+                    <div className={style.overlay}>
+                        <button
+                            className={style.btnCard}
+                            type="button"
+                            onClick={() => this.showModal('tenses')}
                         >
                             SHOW
                         </button>
                     </div>
                 </div>
 
-                <Modal show={this.state.modal} handleClose={this.hideModal}>
-                    <img src={superkassaImg} className={style.imgInModal} alt="superkassa site" />
-                    <div className={style.contentInModal}>
-                        <h2>Superkassa.ru</h2>
-                        <h4>Airtickets sale</h4>
+                {{
+                    superkassa: <Modal show={this.state.modal} handleClose={this.hideModal}>
+                        <img src={superkassaImg} className={style.imgInModal} alt="superkassa site" />
+                        <div className={style.contentInModal}>
+                            <h2>Superkassa.ru</h2>
+                            <h4>Airtickets sale</h4>
 
-                        <p>Roambi provides analytics, reporting, and business intelligence for companies to use on the go. A Wordpress hosted site written in PHP and Javascript with Hubspot Integration.</p>
-                    </div>
-                </Modal>
+                            <p>Roambi provides analytics, reporting, and business intelligence for companies to use on the go. A Wordpress hosted site written in PHP and Javascript with Hubspot Integration.</p>
+                        </div>
+                    </Modal>,
+                    runes: <Modal show={this.state.modal} handleClose={this.hideModal}>
+                        <img src={runesImg} className={style.imgInModal} alt="superkassa site" />
+                        <div className={style.contentInModal}>
+                            <h2>Superkassa.ru</h2>
+                            <h4>Airtickets sale</h4>
+
+                            <p>Roambi provides analytics, reporting, and business intelligence for companies to use on the go. A Wordpress hosted site written in PHP and Javascript with Hubspot Integration.</p>
+                        </div>
+                    </Modal>,
+                    tenses: <Modal show={this.state.modal} handleClose={this.hideModal}>
+                        <img src={tensesGeneratorImg} className={style.imgInModal} alt="superkassa site" />
+                        <div className={style.contentInModal}>
+                            <h2>Superkassa.ru</h2>
+                            <h4>Airtickets sale</h4>
+
+                            <p>Roambi provides analytics, reporting, and business intelligence for companies to use on the go. A Wordpress hosted site written in PHP and Javascript with Hubspot Integration.</p>
+                        </div>
+                    </Modal>
+                }[this.state.modal]}
             </Fragment>
         )
     }
