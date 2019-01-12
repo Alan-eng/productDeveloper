debd3f4d37b07599fb1ec68eb5c87e6ae19b89ad
Alan <alan-eng@yandex.ru>
2019-01-12 18:52:42 +0300

    wip

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import ScrollableAnchor from "react-scrollable-anchor"
+import { FaEnvelope, FaTwitter } from 'react-icons/fa';
 import style from './index.css';
 
 const Contacts = () => (
-    <div className="slide">
-        If you have any question about design service, or just want to say hello, feel free to contact me. You can email me at hey@rleonardi.com, or follow me at ... ;)
-    </div>
+    <section className={style.contacts}>
+        <div>
+            <ScrollableAnchor id={'contacts'}>
+                <h1>Contacts</h1>
+            </ScrollableAnchor >
+            If you have any question, or just want to say hello, feel free to contact me ;) <br />
+            <ul className={style.contactsUl}>
+
+                <li className={style.contact}><FaEnvelope /> alan-eng@yandex.ru</li>
+                {/* <li className={style.contact}><FaTwitter /> alan-eng@yandex.ru</li> */}
+
+            </ul>
+        </div>
+        ©2017 Betanov Alan
+    </section>
 )
 
 export default Contacts
